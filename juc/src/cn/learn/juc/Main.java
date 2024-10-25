de42504b21ba93b75a7ea2224c2bc57e08ba2550
zengxuebin <1812806483@qq.com>
2024-10-25 09:46:16 -0500

    reset main

--- juc/src/cn/learn/juc/Main.java
+++ juc/src/cn/learn/juc/Main.java
@@ -2,30 +2,6 @@ package cn.learn.juc;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "La compra realizada no otorga kWh, Intente con un importe mayor a B/. &";
-        String[] split = message.split("&");
-
-        StringBuffer result = new StringBuffer(message);
-        int count = 0;
-        while (result.indexOf("&") != -1 && count < 4) {
-            int index = result.indexOf("&");
-            switch (count) {
-                case 0:
-                    result.replace(index, index + 1, "         17.00");
-                    break;
-                case 1:
-                    result.replace(index, index + 1, "messageV2");
-                    break;
-                case 2:
-                    result.replace(index, index + 1, "messageV3");
-                    break;
-                case 3:
-                    result.replace(index, index + 1, "messageV4");
-                    break;
-            }
-            count++;
-        }
-        System.out.println(result);
         System.out.println("Hello Juc!");
     }
 }
